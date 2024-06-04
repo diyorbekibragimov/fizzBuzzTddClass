833b2d06a8098d0342885c4dab314191f429f69b
Diyorbek Ibragimov <dierbekibragimov@gmail.com>
2024-06-04 12:03:53 +0300

    changed the test case

--- test_initial.py
+++ test_initial.py
@@ -1,3 +1,11 @@
 from main import process
 def test_number_one():
-    assert(process(1) == 1)
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            assert process(i) == "FizzBuzz"
+        elif i % 3 == 0:
+            assert process(i) == "Fizz"
+        elif i % 5 == 0:
+            assert process(i) == "Buzz"
+        else:
+            assert process(i) == i
